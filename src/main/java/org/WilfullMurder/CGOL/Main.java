c9c731720493571af343b73fecdeaff899cdbc15
wilfullmurder <mrfarrow2013@gmail.com>
2025-01-09 21:43:37 +0000

    Reverts old grid population method in Main
    updates pom.xml to handle manifest

--- src/main/java/org/WilfullMurder/CGOL/Main.java
+++ src/main/java/org/WilfullMurder/CGOL/Main.java
@@ -30,21 +30,15 @@ public class Main {
 
         for (int i = 0; i < grid.getWidth(); i++) {
             for (int j = 0; j < grid.getHeight(); j++) {
-                Cell currentCell = grid.getCell(i, j);
                 // Cell neighbours must be located in the 3x3 (0,0) to (2,2) grid around the cell
                 for (int x = 0; x <= 2; x++) {
                     for (int y = 0; y <= 2; y++) {
                         if(x == 1 && y == 1) continue; // Skip the cell itself
 
-                        int neighbourX = i + x;
-                        int neighbourY = j + y;
-                        if (neighbourX >= 0 && neighbourX < grid.getWidth() && neighbourY >= 0 && neighbourY < grid.getHeight()) {
-                            currentCell.addNeighbour(x + 1, y + 1, grid.getCell(neighbourX, neighbourY));
-                        }
+                        grid.getCell(i, j).addNeighbour(x, y, grid.getCell(i + x - 1, j + y - 1));
                     }
                 }
             }
         }
-        System.out.println("Grid populated");
     }
 }
